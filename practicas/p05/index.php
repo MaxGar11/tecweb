4f5ef4f66609ea7e31e9d1b996b243929a08fd60
MaxGar11 <maximiliano.garciari@alumno.buap.mx>
2024-09-04 20:39:56 -0600

    Se ha realizado el ejercicio 1

---
+++ practicas/p05/index.php
@@ -0,0 +1,12 @@
+<?php
+# EJERCICIO 1
+$variables = [
+    '$_myvar' => 'valida',
+    '$_7var' => 'valida',
+    'myvar' => 'invalida',
+    '$myvar' => 'valida',
+    '$var7' => 'valida',
+    '$_element1' => 'valida',
+    '$house*5' => 'inválida'
+];
+
