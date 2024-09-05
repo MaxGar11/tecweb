28f14adba517bfd9303db563e7b7c9293c9ae5ec
MaxGar11 <maximiliano.garciari@alumno.buap.mx>
2024-09-04 21:25:14 -0600

    Se ha realizado el ejercicio 2

--- practicas/p05/index.php
+++ practicas/p05/index.php
@@ -10,3 +10,26 @@ $variables = [
     '$house*5' => 'inválida'
 ];
 
+# EJERCICIO 2
+
+$a = "ManejadorSQL";
+$b = 'MySQL';
+$c = &$a;
+
+print_r($a);
+echo '<br>';
+print_r($b);
+echo '<br>';
+print_r($c);
+echo '<br>';
+
+
+#  inciso b
+
+$a = "PHP server";
+$b = &$a;
+
+echo $a. '<br>';
+echo $b. '<br>';
+
+?>
